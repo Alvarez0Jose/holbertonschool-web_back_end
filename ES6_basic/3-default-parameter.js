78d3fe7383af3949102f07a36f7450734b05f8d5
Alvarez0Jose <josealejandro220@gmail.com>
2023-09-15 11:54:12 -0400

    adding text to the README

---
+++ ES6_basic/3-default-parameter.js
@@ -0,0 +1,3 @@
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expannsion2019 = 19) {
+    return initialNumber + expansion1989 + expannsion2019;
+}
---
+++ ES6_basic/README.md
@@ -0,0 +1,1 @@
+Here we're going to start learning about ES6
