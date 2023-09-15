a5e35a7bbe3d33edfce7f38c056e901053b5dccf
Alvarez0Jose <josealejandro220@gmail.com>
2023-09-15 13:47:39 -0400

    adding Readme and first 3 tasks

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,53 @@
+/* eslint disable */
+export default function HolbertonCourse {
+    constructor(name, length, students) {
+        this._name = this.validateString(name, 'Name');
+        this._length = this.validateNumber(length, 'Length');
+        this._students = this.validateArray(students, 'Students');
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(newName) {
+        this._name = this.validateString(newName, 'Name');
+    }
+
+    get lenght() {
+        return this._length;
+    }
+
+    set length(newLength) {
+        this._length = this.validateNumber(newLength, 'Length');
+    }
+
+    get students() {
+        return this._students;
+    }
+
+    set student(newStudents) {
+        this._students = this.validateArray(newStudents, 'Students');
+    }
+
+    validateString(value, attribute) {
+        if (typeof value !== 'string') {
+            throw new TypeError(`${attribute} must be a string`);
+        }
+        return value;
+    }
+
+    validateNumber(value, attribute) {
+        if (typeof value !== 'number') {
+            throw new TypeError(`${attribute} must be a number`);
+        }
+        return value;
+    }
+
+    validateArray(value, attribute) {
+        if (!Array.isArray(value)) {
+            throw new TypeError(`${attribute} must be an array`);
+        }
+        return value;
+    }
+}
