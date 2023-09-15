8df70a08d7734f4e9c2805c0e444531c2ea0563b
Alvarez0Jose <josealejandro220@gmail.com>
2023-09-15 15:05:41 -0400

    fixing output

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,12 +1,6 @@
-/* eslint disable */
-import ClassRoom from "./0-classroom.js";
+/* eslint-disable */
+import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-    const rooms = [
-        new ClassRoom(19),
-        new ClassRoom(20),
-        new ClassRoom(34),
-    ];
-    
-    return rooms;
+    return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
 }
