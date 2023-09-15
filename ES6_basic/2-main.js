c220dbe8e980e35996d5533e208b549ab3d7ef4a
Alvarez0Jose <josealejandro220@gmail.com>
2023-09-15 11:44:44 -0400

    adding task 2

---
+++ ES6_basic/2-main.js
@@ -0,0 +1,5 @@
+import getNeighborhoodsList from "./2-arrow.js";
+
+const newNeighborhoodList = new getNeighborhoodsList()
+const res = newNeighborhoodList.addNeighborhood('Noe Valley');
+console.log(res);
