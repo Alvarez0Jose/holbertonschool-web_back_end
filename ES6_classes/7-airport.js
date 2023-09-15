22861e72111b891d31abf0f9ea8bc8126cbef59b
Alvarez0Jose <josealejandro220@gmail.com>
2023-09-15 14:53:58 -0400

    adding tasks

---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,11 @@
+/* eslint disable */
+export default class Airport {
+    constructor(name, code) {
+        this._name = name;
+        this._code = code;
+    }
+
+    toString() {
+        return  `[${this._code}]`;
+    }
+}
