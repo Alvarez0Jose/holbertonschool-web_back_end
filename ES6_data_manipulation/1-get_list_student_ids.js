e71dbf0fd8e354379df01e64c2bf190a6a4e8184
Alvarez0Jose <josealejandro220@gmail.com>
2023-10-12 12:45:17 -0400

    adding tasks and Readme file for data manipulation

---
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,9 @@
+/* eslint-disable */
+export default function getListStudentIds(students) {
+    if (!Array.isArray(students)) {
+      return [];
+    }
+  
+    const map = students.map((mappedObj) => mappedObj.id);
+    return map;
+  }
